9f18d7a7c914be88eb329b9f451bfd633c3d91ac
Marcus Ingram <marcus@ingrams.cloud>
2021-11-06 23:00:04 +1100

    HTTPS redirect

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
+const MODE = process.env.MODE;
 
+function requireHTTPS(req, res, next) {
+    if (!req.secure && MODE != "DEV") {
+      return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+}
+
+app.use(requireHTTPS);
 app.use(express.static('public'));
 
 app.get('/',function(req,res) {
