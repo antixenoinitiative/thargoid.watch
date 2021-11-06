736172e39b0134cdecdf0edb17a62e9a91dcfd02
Marcus Ingram <marcus@ingrams.cloud>
2021-11-06 13:53:21 +1100

    Added various features
    Sorting, Extra fields, improved formatting, more data, links.

--- public/script.js
+++ public/script.js
@@ -21,32 +21,117 @@ function getPresence(presence) {
     }
 }
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function indexOfSmallest(a) {
+    return a.indexOf(Math.min.apply(Math, a));
+}
+
+function getRegion(coords) {
+    let regionDistances = []
+    let regions = [
+        [-78.59375, -149.625, -340.53125], // Pleiades (Merope)
+        [355.3125, -425.96875, -723.03125], // Witch Head (Witch Head Sector IR-W C1-9)
+        [432.625, 2.53125, 288.6875], // Coalsack (Musca Dark Region PJ-P B6-1)
+        [-299.0625, -229.25, -876.125], // California (HIP 18390)
+        [0,0,0] // Sol
+    ]
+    for (let region of regions) {
+        let a = region[0] - coords[0];
+        let b = region[1] - coords[1];
+        let c = region[2] - coords[2];
+
+        let distance = Math.sqrt(a * a + b * b + c * c);
+        regionDistances.push(distance)
+    }
+    let regionid = indexOfSmallest(regionDistances)
+
+    switch (regionid) {
+        case 0:
+            return "Pleiades Nebula"
+        case 1:
+            return "Witch Head Nebula"
+        case 2:
+            return "Coalsack Nebula"
+        case 3:
+            return "California Nebula"
+        case 4:
+            return "The Bubble"
+    }
+}
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+
 request.onload = function() {
     const content = request.response;
-    console.log(content)
     let presence;
+    let region;
+    let inchtml = ``
+
     if (content.message.rows.length === 0) {
-        document.getElementById("incursions").innerHTML = `<article><h1>Incursions</h1><p>There are currently no Thargoid Incursions at this time, please check again later. 🙁</p></article>`;
+        document.getElementById("incursions-section").innerHTML = `<article><h1>Incursions</h1><p>There are currently no Thargoid Incursions at this time, please check again later. 🙁</p></article>`;
         return;
     }
-    let inchtml = `<article><h1>Incursions</h1><p>Systems currently under attack by Thargoid Incursion</p></article>`
-    for (let line of content.message.rows) {
-        if (line.status === true) {
-            console.log(line)
-            presence = getPresence(line.presence)
+    
+    content.message.rows.sort(dynamicSort("name"))
+
+    for (let system of content.message.rows) {
+        if (system.status === true) {
+            console.log(system)
+            presence = getPresence(system.presence)
+
+            // Region
+            if (system.coords == null) {
+                region = ""
+            } else {
+                region = `${getRegion(system.coords)}`
+            }
+
+            // Population
+            if (system.population == null) {
+                system.population = "Unknown";
+            } else {
+                system.population = numberWithCommas(system.population)
+            }
+
+            // Faction
+            if (system.faction == null) {
+                system.faction = "Unknown"
+            } else {
+                system.faction = JSON.parse(system.faction).Name
+            }
+            
             inchtml += `
             <div class="subsection">
-                <div class="subsection-heading">
-                    <h1>${line.name}</h1>
+                <div class="subsection-heading-box">
+                    <h1>${system.name} <span class="regionname">${region}</span></h1>
                     <div id="incstatus" class="status-${presence}">${capitalizeFirstLetter(presence)}</div>
+                </div>
+                <div class="subsection-details-box">
+                    <p><span class="axiorange">Faction:</span> ${system.faction}</p>
+                    <p><span class="axiorange">Population:</span> ${system.population}</p>
                 </div>
             </div>`
         }
     }
-    console.log(inchtml)
     document.getElementById("incursions").innerHTML = inchtml;
 }
