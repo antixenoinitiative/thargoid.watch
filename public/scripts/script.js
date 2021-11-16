d853b5cfeb49cfad9da1da009e3b4b286c029114
Marcus Ingram <marcus@ingrams.cloud>
2021-11-16 14:19:21 +1100

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -34,7 +34,7 @@ function getPresence(presence) {
 }
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    return x.toLocaleString("en-US");
 }
 
 function capitalizeFirstLetter(string) {
